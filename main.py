0786891435629ed21e6bf5a48c0ad4637a471a45
l33tdaniel <danielneugent@icloud.com>
2022-09-08 09:38:45 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# It's important to remember that when working with massive files, we want to keep the main file as empty as possible
+# For now, we can use this as a list of things that we want to add to the assignment
+# if anyone has any ideas, let me know! I'd love to help <3
+
+print("hello world!")
